35e0e3d50805e6ff8dc6a9a7c73d647fbafb2a31
EvanGalea2 <evanjamesgalea@gmail.com>
2026-03-20 04:56:11 +0000

    updated installation shell script and readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Distributed-Diabetes-AI-pipeline
 Apache Spark Machine Learning from an SQL server
+
+To install environment and packages:
+./installation.sh
+
+from then on you can activate the environment with:
+./env.sh
--- installation.sh
+++ installation.sh
@@ -1,1 +1,8 @@
-pip install -r requirements.txt
+#!/bin/bash
+DIR="/env"
+if [ ! -d "$DIR" ]; then
+	echo "New install or no env directory, installing environment"
+	python3 -m venv env
+fi
+	source env/bin/activate
+	pip install -r requirements.txt
